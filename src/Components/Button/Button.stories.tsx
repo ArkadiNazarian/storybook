c56240e398cbb8d6b93088641a2afadafa028fe6
ArkadiNazarian <nazarianarkadi@gmail.com>
2022-02-18 13:15:24 +0330

    first commit

---
+++ src/Components/Button/Button.stories.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./Button";
+
+export default {
+    title: 'Form/Button',
+    component: Button
+}
+
+export const Primary = () =>  <Button variant="Primary" text="create"/>;
+export const Secondary = () =>  <Button variant="Secondary" text="create"/>;
+
+
+   
---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import './Button.css';
+
+interface Myprops {
+    variant: "Primary" | "Secondary",
+    text:string
+}
+
+export function Button(props: Myprops) {
+    let classes = "btn";
+    switch(props.variant){
+        case "Primary": classes += " primary-color";break
+        case "Secondary" : classes += " secondary-color";break
+    }
+    return (
+        <div>
+            <button className={classes}>{props.text}</button>
+        </div>
+    )
+}
